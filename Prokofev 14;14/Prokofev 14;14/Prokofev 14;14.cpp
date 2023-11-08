26f38f668e39bde5346456bb46e76d21e47d0f5e
Chelkameshaet <150122723+Chelkameshaet@users.noreply.github.com>
2023-11-08 12:58:26 +0300

    Add files via upload

---
+++ Prokofev 14;14/Prokofev 14;14/Prokofev 14;14.cpp
@@ -0,0 +1,46 @@
+﻿// Prokofev 14;14.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main() 
+{
+    setlocale(LC_ALL, "");
+    int n;
+    std::cout << "Введите размер массива: ";
+    std::cin >> n;
+
+    if (n <= 0) {
+        std::cout << "Размер массива должен быть положительным числом." << std::endl;
+        return 1;
+    }
+
+    int X[1];
+
+    // Заполнение массива
+    std::cout << "Введите элементы массива:" << std::endl;
+    for (int i = 0; i < n; i++) {
+        std::cin >> X[i];
+    }
+
+    int maxElement = X[0];
+    int minElement = X[0];
+    int maxIndex = 0;
+    int minIndex = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (X[i] > maxElement) {
+            maxElement = X[i];
+            maxIndex = i;
+        }
+        if (X[i] < minElement) {
+            minElement = X[i];
+            minIndex = i;
+        }
+    }
+
+    std::cout << "Максимальный элемент: " << maxElement << ", его индекс: " << maxIndex << std::endl;
+    std::cout << "Минимальный элемент: " << minElement << ", его индекс: " << minIndex << std::endl;
+
+    return 0;
+}
---
+++ prokofev 11;11/prokofev 11;11/prokofev 11;11.cpp
@@ -0,0 +1,38 @@
+﻿// prokofev 11;11.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iostream>
+
+int main() 
+{
+    setlocale(LC_ALL, "");
+    int n;
+    std::cout << "Введите количество чисел в последовательности: ";
+    std::cin >> n;
+
+    if (n <= 0) {
+        std::cout << "Количество чисел должно быть положительным числом." << std::endl;
+        return 1;
+    }
+
+    int min_element;
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        int num;
+        std::cout << "Введите число: ";
+        std::cin >> num;
+
+        if (i == 0 || num < min_element) {
+            min_element = num;
+            count = 1;
+        }
+        else if (num == min_element) {
+            count++;
+        }
+    }
+
+    std::cout << "Минимальное число: " << min_element << std::endl;
+    std::cout << "Количество повторений: " << count << std::endl;
+
+    return 0;
+}
